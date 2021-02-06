5a75d94cdd4279c09bb9652ee6c9f0c627b5a53f
Dave Moten <davidmoten@gmail.com>
2021-02-06 15:29:37 +1100

    toString dev

--- src/main/java/sgb/decoder/rotatingfield/EltDtInFlightEmergency.java
+++ src/main/java/sgb/decoder/rotatingfield/EltDtInFlightEmergency.java
@@ -3,7 +3,10 @@ package sgb.decoder.rotatingfield;
 import java.time.OffsetTime;
 import java.util.Optional;
 
-public final class EltDtInFlightEmergency implements RotatingField {
+import sgb.decoder.HasIndentedToString;
+import sgb.decoder.Indent;
+
+public final class EltDtInFlightEmergency implements RotatingField, HasIndentedToString {
 
     private final OffsetTime timeOfLastEncodedLocationSeconds;
     private final int altitudeEncodedLocationMetres;
@@ -39,6 +42,19 @@ public final class EltDtInFlightEmergency implements RotatingField {
 
     public Optional<Range> remainingBatteryCapacityPercent() {
         return remainingBatteryCapacityPercent;
+    }
+
+    @Override
+    public String toString(Indent indent) {
+        return indent.builder() //
+                .right() //
+                .add("time of last encoded location", timeOfLastEncodedLocationSeconds) //
+                .add("altitude of encoded location (metres)", altitudeEncodedLocationMetres) //
+                .add("triggering event", triggeringEvent) //
+                .add("GNSS status", gnssStatus) //
+                .add("remaining battery capacity percent", remainingBatteryCapacityPercent) //
+                .left() //
+                .toString();
     }
 
 }
