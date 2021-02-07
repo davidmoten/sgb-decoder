dbdd1db9d5ec6060c81a3f2439ee22a74755ede6
Dave Moten <davidmoten@gmail.com>
2021-02-07 15:23:19 +1100

    add toString tests

--- src/test/java/sgb/decoder/rotatingfield/RlsTest.java
+++ src/test/java/sgb/decoder/rotatingfield/RlsTest.java
@@ -1,5 +1,19 @@
 package sgb.decoder.rotatingfield;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Optional;
+
+import org.junit.Test;
+
+import sgb.decoder.TestingUtil;
+
 public class RlsTest {
 
+    @Test
+    public void testToString() {
+        Rls a = new Rls(true, false, RlsProvider.GALILEO, Optional.empty());
+        System.out.println(a.toString());
+        assertEquals(TestingUtil.readResource("/rls-to-string.txt"), a.toString());
+    }
 }
--- src/test/java/sgb/decoder/rotatingfield/UnknownRotatingFieldTest.java
+++ src/test/java/sgb/decoder/rotatingfield/UnknownRotatingFieldTest.java
@@ -1,5 +1,17 @@
 package sgb.decoder.rotatingfield;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import sgb.decoder.TestingUtil;
+
 public class UnknownRotatingFieldTest {
 
+    @Test
+    public void testToString() {
+        UnknownRotatingField a = new UnknownRotatingField("11011");
+        System.out.println(a.toString());
+        assertEquals(TestingUtil.readResource("/unknown-rotating-field-to-string.txt"), a.toString());
+    }
 }
