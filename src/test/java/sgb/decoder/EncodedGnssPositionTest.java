805fccb8cbc085515f62603fb28f86606755ade3
David Moten <davidmoten@gmail.com>
2021-02-10 12:09:03 +1100

    remove unused resources and add test

--- src/test/java/sgb/decoder/EncodedGnssPositionTest.java
+++ src/test/java/sgb/decoder/EncodedGnssPositionTest.java
@@ -2,11 +2,14 @@ package sgb.decoder;
 
 import org.junit.Test;
 
+import sgb.decoder.internal.json.Json;
+
 public class EncodedGnssPositionTest {
     
     @Test
     public void testToString() {
         EncodedGnssPosition a = new EncodedGnssPosition(-43, 140);
+        TestingUtil.assertResourceEqualsJson("/encoded-gnss-position.json", Json.toJson(a));
     }
 
 }
